76bea6a574c80da23a7fc41ebce6f28aa0bddf02
EvanJamesMG <Evan123mg@gmail.com>
2015-12-18 10:39:19 +0800

    Update 053.Maximum Subarray.py

--- python/Dynamic Programming/053.Maximum Subarray.py
+++ python/Dynamic Programming/053.Maximum Subarray.py
@@ -1,4 +1,9 @@
 '''
+Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+
+For example, given the array [−2,1,−3,4,−1,2,1,−5,4],
+the contiguous subarray [4,−1,2,1] has the largest sum = 6.
+
 时间 O(n)   空间O(1)
 
 扫描一遍数组，若当前i，前面i-1的结果若为负的话，新序列就从当前A[i]开始算起了，不然就将当前A[i]附加上去。
