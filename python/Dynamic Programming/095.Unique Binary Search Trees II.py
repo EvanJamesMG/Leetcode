9b1e2514292d09560630c9274f0270de9ccde9d4
EvanJamesMG <Evan123mg@gmail.com>
2015-12-18 11:15:41 +0800

    Update 095.Unique Binary Search Trees II.py

--- python/Dynamic Programming/095.Unique Binary Search Trees II.py
+++ python/Dynamic Programming/095.Unique Binary Search Trees II.py
@@ -1,6 +1,16 @@
-# coding=utf-8
-# Definition for singly-linked list.
+'''
+Given n, generate all structurally unique BST's (binary search trees) that store values 1...n.
 
+For example,
+Given n = 3, your program should return all 5 unique BST's shown below.
+
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
+'''
+# coding=utf-8
 class TreeNode(object):
     def __init__(self, x):
         self.val = x
@@ -47,17 +57,7 @@ class Solution(object):
 
         return result
 
-# Your Codec object will be instantiated and called as such:
-# codec = Codec()
-# codec.deserialize(codec.serialize(root))
-#
 # if __name__ == "__main__":
-#
-#     mnode = ListNode(3)
-#     mnode.next = ListNode(5)
-#     mnode.next.next = ListNode(6)
-#     mnode.next.next.next = ListNode(7)
-#     mnode.next.next.next.next = ListNode(8)
 #
 #     result = Solution().rotateRight(mnode, 6)
 #     print(result.val)
