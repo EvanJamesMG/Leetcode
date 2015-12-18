b1514909612d3388e5a0b03aecb6505bfe4350ed
EvanJamesMG <Evan123mg@gmail.com>
2015-12-18 11:27:56 +0800

    Update 121.Best Time to Buy and Sell Stock.py

--- python/Dynamic Programming/121.Best Time to Buy and Sell Stock.py
+++ python/Dynamic Programming/121.Best Time to Buy and Sell Stock.py
@@ -1,6 +1,10 @@
+'''
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+'''
 # coding=utf-8
 import sys
-
 
 class TreeNode(object):
     def __init__(self, x):
@@ -14,7 +18,6 @@ class ListNode(object):
         self.val = x
         self.next = None
 
-
 '''
 分析：动态规划法。
 
@@ -22,7 +25,6 @@ class ListNode(object):
 
 作为可能的最大收益，整个遍历过程中，出现过的最大收益就是所求。
 '''
-
 
 class Solution(object):
     def maxProfit(self, prices):
@@ -39,16 +41,6 @@ class Solution(object):
             maxprofit = max(maxprofit, prices[i] - curmin)
         return maxprofit
 
-
-# Your NumArray object will be instantiated and called as such:
-# numArray = NumArray(nums)
-# numArray.sumRange(0, 1)
-# numArray.sumRange(1, 2)
-
-# Your Codec object will be instantiated and called as such:
-# codec = Codec()
-# codec.deserialize(codec.serialize(root))
-#
-if __name__ == "__main__":
-    result = Solution().nthUglyNumber(12)
-    print(result)
+# if __name__ == "__main__":
+#     result = Solution().nthUglyNumber(12)
+#     print(result)
