72c1eee8daa237ca804ff361ab798e24f40a23c3
EvanJamesMG <Evan123mg@gmail.com>
2015-12-24 11:36:55 +0800

    update

---
+++ python/Backtracking/017. Letter Combinations of a Phone Number My Submissions Question.py
@@ -0,0 +1,70 @@
+# coding=utf-8
+'''
+Given a set of distinct integers, nums, return all possible subsets.
+
+Note:
+Elements in a subset must be in non-descending order.
+The solution set must not contain duplicate subsets.
+For example,
+If nums = [1,2,3], a solution is:
+
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+'''
+import sys
+
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+'''
+解题思路：穷举所有可能的字符串使用dfs来解决。
+
+'''
+
+
+class Solution:
+    # @return a list of strings, [s1, s2]
+    def letterCombinations(self, digits):
+        def dfs(num, string, res):
+            if num == length:
+                res.append(string)
+                return
+            for letter in dict[digits[num]]:
+                dfs(num + 1, string + letter, res)
+
+        dict = {'2': ['a', 'b', 'c'],
+                '3': ['d', 'e', 'f'],
+                '4': ['g', 'h', 'i'],
+                '5': ['j', 'k', 'l'],
+                '6': ['m', 'n', 'o'],
+                '7': ['p', 'q', 'r', 's'],
+                '8': ['t', 'u', 'v'],
+                '9': ['w', 'x', 'y', 'z']
+                }
+        res = []
+        length = len(digits)
+        dfs(0, '', res)
+        return res
+
+# if __name__ == "__main__":
+#     result = Solution().getPermutation(3, 4)
+#     print(result)
