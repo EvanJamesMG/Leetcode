9e24ffe5ab46cca5f0999dba4cd1dd182a86b8cd
EMG <Evan123mg@gmail.com>
2015-12-24 16:49:17 +0800

    Update 090. Subsets II.py

--- python/Backtracking/090. Subsets II.py
+++ python/Backtracking/090. Subsets II.py
@@ -1,20 +1,18 @@
 # coding=utf-8
 '''
-Given a set of distinct integers, nums, return all possible subsets.
+Given a collection of integers that might contain duplicates, nums, return all possible subsets.
 
 Note:
 Elements in a subset must be in non-descending order.
 The solution set must not contain duplicate subsets.
 For example,
-If nums = [1,2,3], a solution is:
+If nums = [1,2,2], a solution is:
 
 [
-  [3],
-  [1],
   [2],
-  [1,2,3],
-  [1,3],
-  [2,3],
+  [1],
+  [1,2,2],
+  [2,2],
   [1,2],
   []
 ]
