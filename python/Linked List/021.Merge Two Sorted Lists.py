95aa44b821664f4229dbe43ce93579e8aad9510e
EMG <Evan123mg@gmail.com>
2016-01-13 15:03:00 +0800

    Update 021.Merge Two Sorted Lists.py

--- python/Linked List/021.Merge Two Sorted Lists.py
+++ python/Linked List/021.Merge Two Sorted Lists.py
@@ -1,4 +1,9 @@
 # coding=utf-8
+'''
+Merge two sorted linked lists and return it as a new list. 
+
+The new list should be made by splicing together the nodes of the first two lists.
+'''
 class ListNode(object):
     def __init__(self, x):
         self.val = x
