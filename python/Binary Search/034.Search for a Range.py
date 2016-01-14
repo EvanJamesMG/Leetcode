5bd4f808009c6c71f1115ebd7b0ab60774e9483b
EMG <Evan123mg@gmail.com>
2016-01-14 10:45:14 +0800

    Update 034.Search for a Range.py

--- python/Binary Search/034.Search for a Range.py
+++ python/Binary Search/034.Search for a Range.py
@@ -1,6 +1,16 @@
 # coding=utf-8
 import collections
+'''
+Given a sorted array of integers, find the starting and ending position of a given target value.
 
+Your algorithm's runtime complexity must be in the order of O(log n).
+
+If the target is not found in the array, return [-1, -1].
+
+For example,
+Given [5, 7, 7, 8, 8, 10] and target value 8,
+return [3, 4].
+'''
 
 class TreeNode(object):
     def __init__(self, x):
