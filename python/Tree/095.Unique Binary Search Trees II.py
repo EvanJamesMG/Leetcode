18bbc42147c32fd967e8682fdc898c4e5a8b0f3f
EMG <Evan123mg@gmail.com>
2016-01-17 15:20:25 +0800

    Update 095.Unique Binary Search Trees II.py

--- python/Tree/095.Unique Binary Search Trees II.py
+++ python/Tree/095.Unique Binary Search Trees II.py
@@ -1,6 +1,17 @@
 # coding=utf-8
 # Definition for singly-linked list.
+'''
+Given n, generate all structurally unique BST's (binary search trees) that store values 1...n.
 
+For example,
+Given n = 3, your program should return all 5 unique BST's shown below.
+
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
+'''
 class TreeNode(object):
     def __init__(self, x):
         self.val = x
