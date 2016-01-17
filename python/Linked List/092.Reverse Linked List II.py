365554553671635faacbf69c9cd2dac85bae93a3
EMG <Evan123mg@gmail.com>
2016-01-17 12:09:52 +0800

    Update 092.Reverse Linked List II.py

--- python/Linked List/092.Reverse Linked List II.py
+++ python/Linked List/092.Reverse Linked List II.py
@@ -1,4 +1,17 @@
 # coding=utf-8
+'''
+Reverse a linked list from position m to n. Do it in-place and in one-pass.
+
+For example:
+Given 1->2->3->4->5->NULL, m = 2 and n = 4,
+
+return 1->4->3->2->5->NULL.
+
+Note:
+Given m, n satisfy the following condition:
+1 ≤ m ≤ n ≤ length of list.
+'''
+
 # Definition for singly-linked list.
 '''
 将链表分为3段 ，第二段翻转后，前面和第一段相连，后面和第三段相连
