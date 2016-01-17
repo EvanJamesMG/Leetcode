8f03a6f2990bc64ab6b7c7cbe86e047472b13ef9
EMG <Evan123mg@gmail.com>
2016-01-17 11:02:57 +0800

    Update 083.Remove Duplicates from Sorted List.py

--- python/Linked List/083.Remove Duplicates from Sorted List.py
+++ python/Linked List/083.Remove Duplicates from Sorted List.py
@@ -1,4 +1,14 @@
 # coding=utf-8
+'''
+Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+For example,
+Given 1->1->2, return 1->2.
+Given 1->1->2->3->3, return 1->2->3.
+'''
+'''
+链表的基本操作
+'''
 # Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, x):
