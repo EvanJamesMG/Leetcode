0869de311a9ab6e16a333a7d2458c5a22ef724d0
EMG <Evan123mg@gmail.com>
2016-01-18 11:34:37 +0800

    Update 108.Convert Sorted Array to Binary Search Tree.py

--- python/Tree/108.Convert Sorted Array to Binary Search Tree.py
+++ python/Tree/108.Convert Sorted Array to Binary Search Tree.py
@@ -1,6 +1,9 @@
 # coding=utf-8
-# Definition for singly-linked list.
+'''
+Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+'''
 
+# Definition for singly-linked list.
 class TreeNode(object):
     def __init__(self, x):
         self.val = x
