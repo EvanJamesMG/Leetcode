a72719408e3fc73565add1ed90c015552b9129c6
EMG <Evan123mg@gmail.com>
2016-01-18 12:19:46 +0800

    Update 114.Flatten Binary Tree to Linked List.py

--- python/Tree/114.Flatten Binary Tree to Linked List.py
+++ python/Tree/114.Flatten Binary Tree to Linked List.py
@@ -1,6 +1,30 @@
 # coding=utf-8
-# Definition for singly-linked list.
+'''
+Given a binary tree, flatten it to a linked list in-place.
 
+For example,
+Given
+
+         1
+        / \
+       2   5
+      / \   \
+     3   4   6
+The flattened tree should look like:
+   1
+    \
+     2
+      \
+       3
+        \
+         4
+          \
+           5
+            \
+             6
+'''
+
+# Definition for singly-linked list.
 class TreeNode(object):
     def __init__(self, x):
         self.val = x
