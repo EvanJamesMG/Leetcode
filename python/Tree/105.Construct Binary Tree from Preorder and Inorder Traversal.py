3f3384bef99c8e07b8d36e13d9257ccde63bf22c
EMG <Evan123mg@gmail.com>
2016-01-18 11:53:40 +0800

    Update 105.Construct Binary Tree from Preorder and Inorder Traversal.py

--- python/Tree/105.Construct Binary Tree from Preorder and Inorder Traversal.py
+++ python/Tree/105.Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -1,8 +1,12 @@
 # coding=utf-8
+'''
+Given preorder and inorder traversal of a tree, construct the binary tree.
+
+Note:
+You may assume that duplicates do not exist in the tree.
+'''
+
 # Definition for singly-linked list.
-'''
-深度优先搜索
-'''
 class TreeNode(object):
     def __init__(self, x):
         self.val = x
@@ -13,10 +17,10 @@ class ListNode(object):
         self.val = x
         self.next = None
 '''
-  * 根据中序序列和后序序列构造二叉树，后序的最后一个结点一定是根结点，
+  * 根据中序序列和先序序列构造二叉树，后序的最后一个结点一定是根结点，
   * 该结点可以把中序序列分成左右两部分，对应于左右子树的中序序列，
-  * 根据左右两部分中结点的个数又可以把后序序列中除去最后一个结点的序列分成两部分，
-  * 对应于左右子树的后序序列，这样就可以递归构造左右子树。
+  * 根据左右两部分中结点的个数又可以把先序序列中除去头第一个结点的序列分成两部分，
+  * 对应于左右子树的先序序列，这样就可以递归构造左右子树。
 '''
 
 class Solution(object):
