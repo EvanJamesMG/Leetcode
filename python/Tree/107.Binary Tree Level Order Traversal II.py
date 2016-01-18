67be936dd1d76415d0f5ab28c22c730319cd475b
EMG <Evan123mg@gmail.com>
2016-01-18 11:30:22 +0800

    Update 107.Binary Tree Level Order Traversal II.py

--- python/Tree/107.Binary Tree Level Order Traversal II.py
+++ python/Tree/107.Binary Tree Level Order Traversal II.py
@@ -1,5 +1,23 @@
 # coding=utf-8
 '''
+Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
+
+For example:
+Given binary tree {3,9,20,#,#,15,7},
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its bottom-up level order traversal as:
+[
+  [15,7],
+  [9,20],
+  [3]
+]
+'''
+
+'''
 广度优先搜索 + 链表的翻转
 '''
 import Queue
