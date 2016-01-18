587a318afef30252e3d70e7ea60cf163a91dd6e1
EMG <Evan123mg@gmail.com>
2016-01-18 16:09:30 +0800

    Update 119. Pascals Triangle II.py

--- python/Array/119. Pascals Triangle II.py
+++ python/Array/119. Pascals Triangle II.py
@@ -25,19 +25,20 @@ class ListNode(object):
         self.val = x
         self.next = None
 
-
+'''
+此题和《118. Pascal's Triangle》类似，但是需要注意的是，上一题给出的numRows是第几行（从1开始），此题给出的rowIndex是行序号（从0开始）。
+'''
 class Solution:
     # @return a list of integers
     def getRow(self, rowIndex):
         if rowIndex == 0: return [1]
         if rowIndex == 1: return [1, 1]
         list = [[] for i in range(rowIndex + 1)]
-        list[0] = [1]
-        list[1] = [1, 1]
-        for i in range(2, rowIndex + 1):
-            list[i] = [1 for j in range(i + 1)]
+        for i in range(0, rowIndex+1):
+            list[i] = [1 for j in range(i + 1)] #先把每一行都填上一
+        for i in range(2, rowIndex+1):
             for j in range(1, i):
-                list[i][j] = list[i - 1][j - 1] + list[i - 1][j]
+                list[i][j] = list[i - 1][j - 1] + list[i - 1][j]  #中间部分另外计算
         return list[rowIndex]
 
 # Your BSTIterator will be called like this:
