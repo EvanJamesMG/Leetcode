1bcf569d526d52236171b209b2904a49b5b2653c
EMG <Evan123mg@gmail.com>
2016-01-18 11:42:27 +0800

    Update 110.Balanced Binary Tree.py

--- python/Tree/110.Balanced Binary Tree.py
+++ python/Tree/110.Balanced Binary Tree.py
@@ -1,6 +1,10 @@
 # coding=utf-8
-# Definition for singly-linked list.
+'''
+Given a binary tree, determine if it is height-balanced.
 
+For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+'''
+# Definition for singly-linked list.
 class TreeNode(object):
     def __init__(self, x):
         self.val = x
