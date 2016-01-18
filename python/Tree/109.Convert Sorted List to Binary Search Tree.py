dfc7da562e41bd9f9ca95eff76213095b6d62e21
EMG <Evan123mg@gmail.com>
2016-01-18 11:40:00 +0800

    Update 109.Convert Sorted List to Binary Search Tree.py

--- python/Tree/109.Convert Sorted List to Binary Search Tree.py
+++ python/Tree/109.Convert Sorted List to Binary Search Tree.py
@@ -1,5 +1,7 @@
 # coding=utf-8
-
+'''
+Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+'''
 class TreeNode(object):
     def __init__(self, x):
         self.val = x
