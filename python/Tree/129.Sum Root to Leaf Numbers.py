5243bd0fa4e8e41e67a81def587d697436037177
EMG <Evan123mg@gmail.com>
2016-01-20 21:52:29 +0800

    Update 129.Sum Root to Leaf Numbers.py

--- python/Tree/129.Sum Root to Leaf Numbers.py
+++ python/Tree/129.Sum Root to Leaf Numbers.py
@@ -1,8 +1,25 @@
 # coding=utf-8
-# Definition for singly-linked list.
 '''
+Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
+
+An example is the root-to-leaf path 1->2->3 which represents the number 123.
+
+Find the total sum of all root-to-leaf numbers.
+
+For example,
+
+    1
+   / \
+  2   3
+The root-to-leaf path 1->2 represents the number 12.
+The root-to-leaf path 1->3 represents the number 13.
+
+Return the sum = 12 + 13 = 25.
+
 深度优先搜索
 '''
+# Definition for singly-linked list.
+
 class TreeNode(object):
     def __init__(self, x):
         self.val = x
