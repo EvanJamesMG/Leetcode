9794ff086d85ab3c9486d5c50b9656c0672c3021
EMG <Evan123mg@gmail.com>
2016-02-25 16:14:08 +0800

    Update 279.Perfect Squares.java

--- python/Dynamic Programming/279.Perfect Squares.java
+++ python/Dynamic Programming/279.Perfect Squares.java
@@ -1,4 +1,6 @@
 /*
+Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
+For example, given n = 12, return 3 because 12 = 4 + 4 + 4; given n = 13, return 2 because 13 = 4 + 9.
 
 -- Define dp[n + 1], where dp[i] means the least number of perfect square numbers for integer i.
 -- Initialization. dp[0] = 0. dp[i] = Integer.MAX_VALUE since we calculate the min number
