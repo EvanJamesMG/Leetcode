ea00b530d3a99fc51b677114c365cd71329da1d3
EMG <Evan123mg@gmail.com>
2016-02-25 16:40:57 +0800

    Update 309.Best Time to Buy and Sell Stock with Cooldown.py

--- python/Dynamic Programming/309.Best Time to Buy and Sell Stock with Cooldown.py
+++ python/Dynamic Programming/309.Best Time to Buy and Sell Stock with Cooldown.py
@@ -1,14 +1,23 @@
 # coding=utf-8
+'''
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times) with the following restrictions:
+
+You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+After you sell your stock, you cannot buy stock on next day. (ie, cooldown 1 day)
+Example:
+
+prices = [1, 2, 3, 0, 2]
+maxProfit = 3
+transactions = [buy, sell, cooldown, buy, sell]
+'''
 import sys
-
-
 class TreeNode(object):
     def __init__(self, x):
         self.val = x
         self.left = None
         self.right = None
-
-
 class ListNode(object):
     def __init__(self, x):
         self.val = x
@@ -46,16 +55,6 @@ class Solution(object):
 
         return sell[n - 1]
 
-
-# Your NumArray object will be instantiated and called as such:
-# numArray = NumArray(nums)
-# numArray.sumRange(0, 1)
-# numArray.sumRange(1, 2)
-
-# Your Codec object will be instantiated and called as such:
-# codec = Codec()
-# codec.deserialize(codec.serialize(root))
-#
 if __name__ == "__main__":
     result = Solution().nthUglyNumber(12)
     print(result)
