5bc77c2062e52172beb73ae040f0d5c90d88a3e6
EMG <Evan123mg@gmail.com>
2016-02-26 15:35:26 +0800

    Update 100.Same Tree.py

--- python/Depth-first Search/100.Same Tree.py
+++ python/Depth-first Search/100.Same Tree.py
@@ -1,5 +1,9 @@
 # coding=utf-8
+'''
+Given two binary trees, write a function to check if they are equal or not.
 
+Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
+'''
 # class TreeNode(object):
 #     def __init__(self, x):
 #         self.val = x
