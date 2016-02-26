82b2f597c3e6c686d8e7562775eeec0ffaf8ec83
EMG <Evan123mg@gmail.com>
2016-02-26 18:11:57 +0800

    Update 257.Binary Tree Paths.py

--- python/Depth-first Search/257.Binary Tree Paths.py
+++ python/Depth-first Search/257.Binary Tree Paths.py
@@ -1,5 +1,18 @@
 # coding=utf-8
+'''
+Given a binary tree, return all root-to-leaf paths.
 
+For example, given the following binary tree:
+
+   1
+ /   \
+2     3
+ \
+  5
+All root-to-leaf paths are:
+
+["1->2->5", "1->3"]
+'''
 # class TreeNode(object):
 #     def __init__(self, x):
 #         self.val = x
