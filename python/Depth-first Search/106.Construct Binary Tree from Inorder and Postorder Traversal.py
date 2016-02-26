813a87ae9ef025a940326c45c31cd7b46bb8e797
EMG <Evan123mg@gmail.com>
2016-02-26 17:10:58 +0800

    Update 106.Construct Binary Tree from Inorder and Postorder Traversal.py

--- python/Depth-first Search/106.Construct Binary Tree from Inorder and Postorder Traversal.py
+++ python/Depth-first Search/106.Construct Binary Tree from Inorder and Postorder Traversal.py
@@ -1,4 +1,10 @@
 # coding=utf-8
+'''
+Given inorder and traversal of a tree, construct the binary tree.
+
+Note:
+You may assume that duplicates do not exist in the tree.
+'''
 # Definition for singly-linked list.
 '''
 深度优先搜索
@@ -26,34 +32,12 @@ class Solution(object):
         :type postorder: List[int]
         :rtype: TreeNode
         """
-        hmap = {}
-        if (inorder ==None and postorder ==None) or (len(inorder) == 0 and len(postorder) ==0):
+        if len(inorder) == 0:
             return None
-        for i in range(len(inorder)):
-            hmap[inorder[i]] = i
-        return self.mhelp(inorder, 0, len(inorder)-1, postorder, 0, len(postorder)-1, hmap)
-
-    def mhelp(self, inorder, inL, inR, postorder, pL, pR, hmap):
-        if inL > inR or pL > pR:
-            return None
-        root = TreeNode(postorder[pR])
-        index = hmap.get(root.val)
-        root.left = self.mhelp(inorder, inL, index-1, postorder, pL, pL+index-inL-1, hmap)
-        root.right = self.mhelp(inorder, index+1, inR, postorder, pR-inR+index,pR-1, hmap)
+        if len(inorder) == 1:
+            return TreeNode(inorder[0])
+        root = TreeNode(postorder[len(postorder) - 1])
+        index = inorder.index(postorder[len(postorder) - 1])
+        root.left = self.buildTree(inorder[ 0 : index ], postorder[ 0 : index ])
+        root.right = self.buildTree(inorder[ index + 1 : len(inorder) ], postorder[ index : len(postorder) - 1 ])
         return root
-
-# Your Codec object will be instantiated and called as such:
-# codec = Codec()
-# codec.deserialize(codec.serialize(root))
-#
-# if __name__ == "__main__":
-#
-#     mnode = ListNode(3)
-#     mnode.next = ListNode(5)
-#     mnode.next.next = ListNode(6)
-#     mnode.next.next.next = ListNode(7)
-#     mnode.next.next.next.next = ListNode(8)
-#
-#     result = Solution().rotateRight(mnode, 6)
-#     print(result.val)
-#
