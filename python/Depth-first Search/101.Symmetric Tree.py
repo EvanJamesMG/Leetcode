3e594b28c76b7711e03832af6a6238902ac934f2
EMG <Evan123mg@gmail.com>
2016-02-26 15:45:45 +0800

    Update 101.Symmetric Tree.py

--- python/Depth-first Search/101.Symmetric Tree.py
+++ python/Depth-first Search/101.Symmetric Tree.py
@@ -1,4 +1,22 @@
 # coding=utf-8
+'''
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+But the following is not:
+    1
+   / \
+  2   2
+   \   \
+   3    3
+'''
+
 # Definition for singly-linked list.
 
 class TreeNode(object):
