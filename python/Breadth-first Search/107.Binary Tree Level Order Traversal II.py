c2167ce65dc431798e1ac110185481edd2397961
EMG <Evan123mg@gmail.com>
2016-02-27 20:29:04 +0800

    Update 107.Binary Tree Level Order Traversal II.py

--- python/Breadth-first Search/107.Binary Tree Level Order Traversal II.py
+++ python/Breadth-first Search/107.Binary Tree Level Order Traversal II.py
@@ -1,5 +1,23 @@
 # coding=utf-8
 '''
+Given a binary tree, return the bottom-up level order traversal of its nodes' values. 
+(ie, from left to right, level by level from leaf to root).
+
+For example:
+Given binary tree {3,9,20,#,#,15,7},
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its bottom-up level order traversal as:
+[
+  [15,7],
+  [9,20],
+  [3]
+]
+'''
+'''
 广度优先搜索 + 链表的翻转
 '''
 import Queue
@@ -37,20 +55,3 @@ class Solution(object):
             reslist.append(temlist)
         reslist.reverse()
         return reslist
-
-
-# Your Codec object will be instantiated and called as such:
-# codec = Codec()
-# codec.deserialize(codec.serialize(root))
-#
-if __name__ == "__main__":
-
-    mnode = TreeNode(1)
-    mnode.left = TreeNode(2)
-    mnode.right = TreeNode(3)
-    mnode.left.left = TreeNode(4)
-    mnode.left.right = TreeNode(5)
-
-    result = Solution().levelOrderBottom(mnode)
-    print(result)
-
