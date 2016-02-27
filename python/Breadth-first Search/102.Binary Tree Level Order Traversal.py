c84d894788fe4849411a97e181d73da8f530049d
EMG <Evan123mg@gmail.com>
2016-02-27 20:17:12 +0800

    Update 102.Binary Tree Level Order Traversal.py

--- python/Breadth-first Search/102.Binary Tree Level Order Traversal.py
+++ python/Breadth-first Search/102.Binary Tree Level Order Traversal.py
@@ -1,4 +1,21 @@
 # coding=utf-8
+'''
+Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+For example:
+Given binary tree {3,9,20,#,#,15,7},
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its level order traversal as:
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+'''
 # Definition for singly-linked list.
 
 class TreeNode(object):
@@ -70,19 +87,4 @@ class Solution:
         return res
 
 
-
-# Your Codec object will be instantiated and called as such:
-# codec = Codec()
-# codec.deserialize(codec.serialize(root))
-#
-if __name__ == "__main__":
-
-    mnode = ListNode(3)
-    mnode.next = ListNode(5)
-    mnode.next.next = ListNode(6)
-    mnode.next.next.next = ListNode(7)
-    mnode.next.next.next.next = ListNode(8)
-
-    result = Solution().rotateRight(mnode, 6)
-    print(result.val)
 
