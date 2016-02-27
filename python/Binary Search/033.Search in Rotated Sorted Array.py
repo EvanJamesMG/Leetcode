1034d6fedb2a81a2e74f8380b49082a3be1ca42d
EMG <Evan123mg@gmail.com>
2016-02-27 21:19:06 +0800

    Update 033.Search in Rotated Sorted Array.py

--- python/Binary Search/033.Search in Rotated Sorted Array.py
+++ python/Binary Search/033.Search in Rotated Sorted Array.py
@@ -24,6 +24,10 @@ class ListNode(object):
 '''
 由于旋转前数组有序，故旋转后，数组被分为两个有序的子区间（无旋转时为一个有序区间）。
 算法同样采用折半查找的思想进行搜索，但上下界first、last的更新相比常规的折半查找略微复杂。
+    关键点在于确定mid位于旋转点之前，还是之后：
+    若mid在旋转点之前left和mid之间的元素都有序（在一个有序子区间中）。例如 [4 5 6 7 8 9  || 1 2 3]
+    若mid在旋转点之后mid和right之间的元素都有序（在一个有序子区间中）。例如 [7 8 || 1 2 3 4 5 6]
+    
 参考 http://zhangxc.com/2013/11/leetcode-search-in-rotated-sorted-array
 '''
 class Solution(object):
@@ -38,23 +42,14 @@ class Solution(object):
             mid = (left + right) / 2
             if target == A[mid]:
                 return mid
-            if A[left] <= A[mid]:
-                if A[left] <= target and target < A[mid]:
+            if A[left] <= A[mid]: #此时mid处于旋转点之前,left和mid之间的元素都有序（在一个有序子区间中）。例如 [4 5 6 7 8 9  || 1 2 3]
+                if A[left] <= target and target < A[mid]:#若if成立，则target在[left,mid)之间，否则在[mid+1,right)之间
                     right = mid - 1
                 else:
                     left = mid + 1
-            else:
-                if  A[mid] < target and target <= A[right]:
+            else:                 #此时mid处于旋转点之后,mid和right之间的元素有序， 例如[7 8 || 1 2 3 4 5 6]
+                if  A[mid] < target and target <= A[right]:#若if成立，则target在(mid,right]之间，否则在[left,mid)之间
                     left = mid + 1
                 else:
                     right = mid - 1
         return -1
-
-
-# Your Codec object will be instantiated and called as such:
-# codec = Codec()
-# codec.deserialize(codec.serialize(root))
-#
-if __name__ == "__main__":
-    result = Solution().canFinish(3, [[0, 2], [2, 1], [1, 0]])
-    print(result)
