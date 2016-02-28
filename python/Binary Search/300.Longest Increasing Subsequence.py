1f7f104e7f954a71a049fa9ce3c9dd6f5ebc173e
EMG <Evan123mg@gmail.com>
2016-02-28 10:57:27 +0800

    Update 300.Longest Increasing Subsequence.py

--- python/Binary Search/300.Longest Increasing Subsequence.py
+++ python/Binary Search/300.Longest Increasing Subsequence.py
@@ -1,19 +1,26 @@
 # coding=utf-8
+'''
+Given an unsorted array of integers, find the length of longest increasing subsequence.
+
+For example,
+Given [10, 9, 2, 5, 3, 7, 101, 18],
+The longest increasing subsequence is [2, 3, 7, 101], therefore the length is 4. 
+Note that there may be more than one LIS combination, it is only necessary for you to return the length.
+
+Your algorithm should run in O(n2) complexity.
+
+Follow up: Could you improve it to O(n log n) time complexity?
+'''
 import collections
-
-
 class TreeNode(object):
     def __init__(self, x):
         self.val = x
         self.left = None
         self.right = None
-
-
 class ListNode(object):
     def __init__(self, x):
         self.val = x
         self.next = None
-
 
 '''
 http://segmentfault.com/a/1190000003819886
@@ -48,11 +55,3 @@ class Solution(object):
                 if nums[x] > nums[y]:
                     dp[x] = max(dp[x], dp[y] + 1)
         return max(dp) if dp else 0
-
-# Your Codec object will be instantiated and called as such:
-# codec = Codec()
-# codec.deserialize(codec.serialize(root))
-#
-if __name__ == "__main__":
-    result = Solution().canFinish(3, [[0, 2], [2, 1], [1, 0]])
-    print(result)
