ce91842cf93dae45454a3a0483a07cd35a301d13
EMG <Evan123mg@gmail.com>
2016-02-28 10:53:59 +0800

    Update 162.Find Peak Element.py

--- python/Binary Search/162.Find Peak Element.py
+++ python/Binary Search/162.Find Peak Element.py
@@ -1,7 +1,16 @@
 # coding=utf-8
+'''
+A peak element is an element that is greater than its neighbors.
+
+Given an input array where num[i] ≠ num[i+1], find a peak element and return its index.
+
+The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
+
+You may imagine that num[-1] = num[n] = -∞.
+
+For example, in array [1, 2, 3, 1], 3 is a peak element and your function should return the index number 2.
+'''
 import collections
-
-
 class TreeNode(object):
     def __init__(self, x):
         self.val = x
@@ -14,16 +23,18 @@ class ListNode(object):
         self.val = x
         self.next = None
 
-
 '''
 二分查找  O(logn)
 
-有人可能有疑问，如果有多个波峰，那么二分查找还有效吗？对本题而言，依然有效，因为题目只让你返回一个peak，不是所有的peak。满足下面的代码条件的点必然是peak中的一个。
+有人可能有疑问，如果有多个波峰，那么二分查找还有效吗？对本题而言，依然有效，因为题目只让你返回一个peak，不是所有的peak。
 
-如果中间元素大于其相邻后续元素，则中间元素左侧(包含该中间元素）必包含一个局部最大值。如果中间元素小于其相邻后续元素，则中间元素右侧必包含一个局部最大值。
+满足下面的代码条件的点必然是peak中的一个。
+
+如果中间元素大于其相邻后续元素，则中间元素左侧(包含该中间元素）必包含一个局部最大值。
+
+如果中间元素小于其相邻后续元素，则中间元素右侧必包含一个局部最大值。
 
 '''
-
 
 class Solution(object):
     def findPeakElement(self, nums):
@@ -42,12 +53,3 @@ class Solution(object):
             else:
                 high = mid
 
-
-
-# Your Codec object will be instantiated and called as such:
-# codec = Codec()
-# codec.deserialize(codec.serialize(root))
-#
-if __name__ == "__main__":
-    result = Solution().canFinish(3, [[0, 2], [2, 1], [1, 0]])
-    print(result)
