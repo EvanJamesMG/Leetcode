adffffa1283a4bdf3904779302ba842ec4ea35ed
EMG <Evan123mg@gmail.com>
2016-03-04 11:02:55 +0800

    Update 035.Search Insert Position.py

--- python/Array/035.Search Insert Position.py
+++ python/Array/035.Search Insert Position.py
@@ -1,6 +1,17 @@
 # coding=utf-8
 import collections
+'''
+Given a sorted array and a target value, return the index if the target is found. 
+If not, return the index where it would be if it were inserted in order.
 
+You may assume no duplicates in the array.
+
+Here are few examples.
+[1,3,5,6], 5 → 2
+[1,3,5,6], 2 → 1
+[1,3,5,6], 7 → 4
+[1,3,5,6], 0 → 0
+'''
 
 class TreeNode(object):
     def __init__(self, x):
