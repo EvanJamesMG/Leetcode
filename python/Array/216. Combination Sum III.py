abac1a61cfb855280c6ef73c5cd3e482ddce20ae
EMG <Evan123mg@gmail.com>
2016-03-04 17:43:24 +0800

    Update 216. Combination Sum III.py

--- python/Array/216. Combination Sum III.py
+++ python/Array/216. Combination Sum III.py
@@ -1,9 +1,10 @@
 # coding=utf-8
 '''
-Find all possible combinations of k numbers that add up to a number n, given that only numbers from 1 to 9 can be used and each combination should be a unique set of numbers.
+Find all possible combinations of k numbers that add up to a number n, 
+
+given that only numbers from 1 to 9 can be used and each combination should be a unique set of numbers.
 
 Ensure that numbers within the set are sorted in ascending order.
-
 
 Example 1:
 
