a04db33a32048f08009fabf2e94fcc900473c568
EMG <Evan123mg@gmail.com>
2016-03-04 15:44:56 +0800

    Update 079. Word Search.py

--- python/Array/079. Word Search.py
+++ python/Array/079. Word Search.py
@@ -1,8 +1,8 @@
 # coding=utf-8
 '''
 Given a 2D board and a word, find if the word exists in the grid.
-
-The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
+The word can be constructed from letters of sequentially adjacent cell, 
+where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
 
 For example,
 Given board =
